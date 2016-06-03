0b6d243219b7bdd121bf91b40ac8bf0d663e7cb7
Ben Alex <ben.alex@acegi.com.au>
2016-06-03 19:53:02 +1000

    Crude performance test with cursors

--- src/main/java/org/deephacks/lmdbjni/Database.java
+++ src/main/java/org/deephacks/lmdbjni/Database.java
@@ -1,10 +1,17 @@
 package org.deephacks.lmdbjni;
 
+import static java.lang.Integer.BYTES;
 import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Math.max;
 import org.bytedeco.javacpp.Pointer;
 import static org.deephacks.lmdbjni.LMDB.*;
 
 import java.nio.ByteBuffer;
+import static java.nio.ByteBuffer.allocateDirect;
+import static java.nio.ByteOrder.LITTLE_ENDIAN;
+import java.util.zip.CRC32;
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
 import org.bytedeco.javacpp.BytePointer;
 
 import static org.deephacks.lmdbjni.LMDB.mdb_get;
@@ -13,28 +20,72 @@ import static org.deephacks.lmdbjni.Util.checkRc;
 
 public class Database {
 
+  private static MDB_val allocateMDB_val(byte[] value) {
+    final BytePointer bp = new BytePointer(value);
+    final MDB_val val = new MDB_val();
+    val.mv_size(value.length);
+    val.mv_data(bp);
+    return val;
+  }
+
   int[] dbi;
 
   public Database(int[] dbi) {
     this.dbi = dbi;
   }
 
-  public void put(Transaction tx, byte[] key, byte[] val) {
-    final MDB_val k = allocateMDB_val(key);
-    final MDB_val v = allocateMDB_val(val);
-    checkRc(mdb_put(tx.tx, dbi[0], k, v, 0));
+  public long crcViaByteBuffer(Transaction tx) {
+    final MDB_cursor cursor = new MDB_cursor();
+    checkRc(mdb_cursor_open(tx.tx, dbi[0], cursor));
+
+    final MDB_val k = new MDB_val();
+    final MDB_val v = new MDB_val();
+
+    final CRC32 crc32 = new CRC32();
+    while (mdb_cursor_get(cursor, k, v, MDB_NEXT) == 0) {
+      assert k.mv_size() > 0;
+      assert v.mv_size() > 0;
+
+      final Pointer keyData = k.mv_data();
+      keyData.capacity(k.mv_size());
+
+      final Pointer valData = v.mv_data();
+      valData.capacity(v.mv_size());
+
+      final ByteBuffer keyBb = keyData.asByteBuffer();
+      final ByteBuffer valBb = valData.asByteBuffer();
+      crc32.update(keyBb);
+      crc32.update(valBb);
+    }
+    return crc32.getValue();
   }
 
-  public void put(Transaction tx, ByteBuffer key, ByteBuffer val) {
+  public long crcViaDirectBuffer(Transaction tx) {
+    final MDB_cursor cursor = new MDB_cursor();
+    checkRc(mdb_cursor_open(tx.tx, dbi[0], cursor));
+
     final MDB_val k = new MDB_val();
-    k.mv_size(key.limit());
-    k.mv_data(new Pointer(key));
-
     final MDB_val v = new MDB_val();
-    v.mv_size(val.limit());
-    v.mv_data(new Pointer(val));
 
-    checkRc(mdb_put(tx.tx, dbi[0], k, v, 0));
+    final MutableDirectBuffer mdb = new UnsafeBuffer(new byte[0]);
+    final CRC32 crc32 = new CRC32();
+
+    while (mdb_cursor_get(cursor, k, v, MDB_NEXT) == 0) {
+      assert k.mv_size() > 0;
+      assert v.mv_size() > 0;
+
+      long biggest = max(k.mv_size(), v.mv_size());
+      byte[] scratch = new byte[(int) biggest];
+
+      wrap(mdb, k);
+      mdb.getBytes(0, scratch);
+      crc32.update(scratch, 0, (int) k.mv_size());
+
+      wrap(mdb, v);
+      mdb.getBytes(0, scratch);
+      crc32.update(scratch, 0, (int) v.mv_size());
+    }
+    return crc32.getValue();
   }
 
   public ByteBuffer get(Transaction tx, ByteBuffer key) {
@@ -78,12 +129,47 @@ public class Database {
     return vBytes;
   }
 
-  private static MDB_val allocateMDB_val(byte[] value) {
-    final BytePointer bp = new BytePointer(value);
-    final MDB_val val = new MDB_val();
-    val.mv_size(value.length);
-    val.mv_data(bp);
-    return val;
+  public void insertData(final Transaction tx, final Database db1,
+                         final int opCount) {
+    final ByteBuffer key = allocateDirect(BYTES);
+    key.order(LITTLE_ENDIAN);
+
+    final ByteBuffer val = allocateDirect(BYTES);
+    val.order(LITTLE_ENDIAN);
+
+    for (int k = 0; k <= opCount; k++) {
+      key.clear();
+      val.clear();
+      key.putInt(k).flip();
+      val.putInt(k).flip();
+      db1.put(tx, key, val);
+    }
+  }
+
+  public void put(Transaction tx, byte[] key, byte[] val) {
+    final MDB_val k = allocateMDB_val(key);
+    final MDB_val v = allocateMDB_val(val);
+    checkRc(mdb_put(tx.tx, dbi[0], k, v, 0));
+  }
+
+  public void put(Transaction tx, ByteBuffer key, ByteBuffer val) {
+    final MDB_val k = new MDB_val();
+    k.mv_size(key.limit());
+    k.mv_data(new Pointer(key));
+
+    final MDB_val v = new MDB_val();
+    v.mv_size(val.limit());
+    v.mv_data(new Pointer(val));
+
+    checkRc(mdb_put(tx.tx, dbi[0], k, v, 0));
+  }
+
+  private void wrap(MutableDirectBuffer mdb, MDB_val mdbVal) {
+    final long size = mdbVal.mv_size();
+    if (size > MAX_VALUE) {
+      throw new UnsupportedOperationException("Value too large for Agrona");
+    }
+    mdb.wrap(mdbVal.mv_data().address(), (int) size);
   }
 
 }
--- src/test/java/org/deephacks/lmdbjni/LMDBTest.java
+++ src/test/java/org/deephacks/lmdbjni/LMDBTest.java
@@ -3,12 +3,15 @@ package org.deephacks.lmdbjni;
 import java.io.File;
 import java.io.IOException;
 import static java.lang.Integer.BYTES;
+import static java.lang.System.nanoTime;
 import static java.lang.System.setProperty;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
 import static java.nio.ByteBuffer.allocateDirect;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
@@ -16,6 +19,9 @@ import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
 public class LMDBTest {
+
+  private static final int KEY_COUNT = 1_000;
+  private static final int RUNS = 10_000;
 
   static {
     setProperty("java.library.path", "./target/classes");
@@ -31,6 +37,50 @@ public class LMDBTest {
   public void before() throws IOException {
     final File path = tmp.newFolder();
     env = new Env(path.getAbsolutePath());
+  }
+
+  @Test
+  public void testCrc32ByByteBuffer() {
+    final Transaction tx = env.openWriteTx();
+    final Database db1 = env.openDatabase(tx, DB_NAME);
+    db1.insertData(tx, db1, KEY_COUNT);
+
+    // check the CRCs are symmetrical
+    final long crcFromDb = db1.crcViaDirectBuffer(tx);
+    assertThat(db1.crcViaByteBuffer(tx), is(crcFromDb));
+
+    // run the cursor speed test (hacky: move to JMH)
+    final long start = nanoTime();
+    int sum = 0;
+    for (int i = 0; i < RUNS; i++) {
+      sum += db1.crcViaByteBuffer(tx);
+    }
+    final long finish = nanoTime();
+    final long runtime = finish - start;
+    System.out.println("BB: " + MILLISECONDS.convert(runtime, NANOSECONDS));
+    tx.commit();
+  }
+
+  @Test
+  public void testCrc32ByDirectBuffer() {
+    final Transaction tx = env.openWriteTx();
+    final Database db1 = env.openDatabase(tx, DB_NAME);
+    db1.insertData(tx, db1, KEY_COUNT);
+
+    // check the CRCs are symmetrical
+    final long crcFromDb = db1.crcViaDirectBuffer(tx);
+    assertThat(db1.crcViaByteBuffer(tx), is(crcFromDb));
+
+    // run the cursor speed test (hacky: move to JMH)
+    final long start = nanoTime();
+    int sum = 0;
+    for (int i = 0; i < RUNS; i++) {
+      sum += db1.crcViaDirectBuffer(tx);
+    }
+    final long finish = nanoTime();
+    final long runtime = finish - start;
+    System.out.println("DB: " + MILLISECONDS.convert(runtime, NANOSECONDS));
+    tx.commit();
   }
 
   @Test
